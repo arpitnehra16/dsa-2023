5a65cf6510c210f7e5ce33f5bc0ef1d7fc18b0b5
arpitnehra16 <arpitnehra1116@gmail.com>
2023-01-05 22:39:45 +0530

    1

--- 1_Reverse__Array.c
+++ 1_Reverse__Array.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int *Reverse_arr( int* a , int n ){
-    int b[n];
-    int i ,j=0;
-    
-    for( i = n-1 ; i >= 0 ; i --){
+int *Reverse_arr(int *a, int *b, int n)
+{
+    // int b[n];
+    int i, j = 0;
+
+    for (i = n - 1; i >= 0; i--)
+    {
         b[j++] = a[i];
     }
     // for( i = 0 ; i < n ; i ++){
@@ -21,37 +23,37 @@ int *Reverse_arr( int* a , int n ){
     return b;
 }
 
-
 int main()
 {
-    int n , i ;
+    int n, i;
     // int * a , *b;
 
     printf("Enter the Number of Elements : ");
-    scanf("%d",&n);
-    int a[n],b[n];
+    scanf("%d", &n);
+    int a[n], b[n];
 
     // a = (int *)malloc( n * sizeof(int));
     // b = (int *)malloc( n * sizeof(int));
 
-    if( a == NULL )
+    if (a == NULL)
     {
         printf("Memmory is not allocated to ARRAY ");
     }
 
     printf("Enter the numbers in ARRAY\n\n");
 
-    for( i = 0 ; i < n ; i++ )
+    for (i = 0; i < n; i++)
     {
-        printf("Enter %d element : ", i+1);
+        printf("Enter %d element : ", i + 1);
         scanf("%d", &a[i]);
     }
     printf("\n\n");
 
-    b = Reverse_arr( a , n );
-     for(i = 0 ; i < n ; i++){
-        printf("%d \n",b[i]);
+    Reverse_arr(a, b, n);
+    for (i = 0; i < n; i++)
+    {
+        printf("%d \n", b[i]);
     }
 
-   return 0 ;
+    return 0;
 }

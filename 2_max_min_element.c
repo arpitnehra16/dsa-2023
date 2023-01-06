245f1563b648f6429e3dd33d9217b57dbf5ca30d
arpitnehra16 <arpitnehra1116@gmail.com>
2023-01-06 20:47:46 +0530

    new

--- 2_max_min_element.c
+++ 2_max_min_element.c
@@ -1,3 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
+// Given an array A of size N of integers. Your task is to find the sum of minimum and maximum element in the array.
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int Max_element(int *a, int n)
+{
+    int max = a[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (max <= a[i])
+        {
+            max = a[i];
+        }
+    }
+    return max;
+}
+
+int Min_element(int *a, int n)
+{
+    int min = a[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (min >= a[i])
+        {
+            min = a[i];
+        }
+    }
+    return min;
+}
+
+int main()
+{
+    int n, Max, Min, Sum;
+    printf("Enter the size of array : ");
+    scanf("%d", &n);
+    int a[n];
+
+    printf("Enter the elements in Array : \n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the %d element : ", i + 1);
+        scanf("%d", &a[i]);
+    }
+    printf("\n");
+
+    Max = Max_element(a, n);
+    Min = Min_element(a, n);
+
+    printf("The Max Element is : %d \n", Max);
+    printf("The Min element is : %d \n", Min);
+
+    printf("The Sum of Maximum And Minimum element is : %d ", Max + Min);
+
+    return 0;
+}
